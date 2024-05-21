581fb9f23713275ea6a93b7135f3e9560a77aaa7
Kashyap <kashyapvenattu@gmail.com>
2024-05-21 00:10:50 -0600

    update contact details for git and linkedin

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,18 +13,22 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:myemail@email.com">myemail@email.com</a>
+          <a href="mailto:kashyapvenattu@gmail.com">kashyapvenattu@gmail.com</a>
         </li>
         <li className={styles.link}>
           <img
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/myname">linkedin.com/myname</a>
+          <a href="https://www.linkedin.com/in/kashyapvenattu/">
+            linkedin.com/in/kashyapvenattu/
+          </a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://www.github.com/myname">github.com/myname</a>
+          <a href="https://www.github.com/kashyapvenattu">
+            github.com/kashyapvenattu
+          </a>
         </li>
       </ul>
     </footer>
